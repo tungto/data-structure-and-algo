0189ce785da35a94871d1c32ab13e86d1ed91536
tungto <tung.to@informa.com>
2024-03-27 19:55:15 +0700

    boyer moor majority vote algo

---
+++ 25_hash-tables/169Majority.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ */
+
+//  using the Boyer-Moore majority vote algorithm is the most optimal solution.
+function majorityElement(nums) {
+	let candidate;
+	let count = 0;
+
+	for (const num of nums) {
+		if (count === 0) {
+			candidate = num;
+		}
+
+		count += num === candidate ? 1 : -1;
+	}
+
+	return candidate;
+}
